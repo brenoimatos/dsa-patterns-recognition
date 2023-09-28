6ddb949c311d997fa31df14691ec055e6464e6b0
brenoimatos <70553244+brenoimatos@users.noreply.github.com>
2023-09-28 13:59:57 -0300

    fix: removing log

--- dsa-patterns/pages/api/list.ts
+++ dsa-patterns/pages/api/list.ts
@@ -4,7 +4,6 @@ import { getUserCount } from "../../lib/api/user";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const results = await getUserCount()
-        console.log(results)
         res.status(200).json({ results })
     } catch(e: any) {
         res.status(500).json({ error: e.message })
