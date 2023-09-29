dac4484d1a6a587dc7e6c46e36cb05323b071dc1
brenoimatos <70553244+brenoimatos@users.noreply.github.com>
2023-09-29 20:57:35 -0300

    feat: multiple choice with simple css

---
+++ dsa-patterns/components/MultipleChoice.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { QuestionProps } from "../lib/api/question";
+import styles from "../styles/MultipleChoice.module.css";
+
+const MultipleChoice = ({ question }: { question: QuestionProps }) => {
+    const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
+    const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+    const choices = ['Array', 'DP', 'Backtracking', 'Graph', 'DFS', 'BFS'];
+
+    const handleChoiceSelection = (choice: string) => {
+        setSelectedChoice(choice);
+
+        if (question.topicTags.some(tag => tag.name === choice)) {
+            setIsCorrect(true);
+        } else {
+            setIsCorrect(false);
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <h2>Multiple Choice</h2>
+            <h3>{question.topicTags.map(tag => tag.name).join(', ')}</h3>
+            <div className={styles.questions}>
+    {choices.map((choice, index) => (
+        <button
+            key={index}
+            onClick={() => handleChoiceSelection(choice)}
+            className={[
+                styles.choice,
+                selectedChoice === choice ? 
+                (isCorrect ? styles.correct : styles.incorrect) : ''
+            ].join(' ')}
+        >
+            {choice}
+        </button>
+    ))}
+</div>
+
+        </div>
+    );
+};
+
+export default MultipleChoice;
--- dsa-patterns/pages/index.tsx
+++ dsa-patterns/pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
-import QuestionCard from '../components/Question'
+import QuestionCard from '../components/QuestionCard'
 import { QuestionProps, getRandomQuestion } from '../lib/api/question'
+import MultipleChoice from '../components/MultipleChoice'
+import styles from '../styles/Home.module.css'
 
 type ConnectionStatus = {
   isConnected: boolean,
@@ -41,9 +43,12 @@ export default function Home({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="title">
-          Welcome to LeetCode Pattern Recognition!
+        <div className={styles.title}>
+        <h1 >
+          LeetCode Pattern Recognition
         </h1>
+        </div>
+        
         {isConnected ? (
           <h2 className="subtitle"></h2>
         ) : (
@@ -52,7 +57,10 @@ export default function Home({
             for instructions.
           </h2>
         )}
+        <div className={styles.grid}>
         {randomQuestion ? <QuestionCard question={randomQuestion} /> : "Loading..."}
+        {randomQuestion ? <MultipleChoice question={randomQuestion} /> : "Loading..."}
+        </div>
         </main>
     </div>
   )
