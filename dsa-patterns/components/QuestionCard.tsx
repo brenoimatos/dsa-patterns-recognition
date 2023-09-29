54589bb3fc93922fff9aa60cce4f45c6f0e29a27
brenoimatos <70553244+brenoimatos@users.noreply.github.com>
2023-09-29 20:57:02 -0300

    refactor: file name

---
+++ dsa-patterns/components/QuestionCard.tsx
@@ -0,0 +1,14 @@
+import { QuestionProps } from "../lib/api/question"
+import styles from "../styles/QuestionCard.module.css"
+
+const QuestionCard = ({ question }: { question: QuestionProps }) => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>{question.title}</h2>
+      <p className={styles.difficulty}>({question.difficulty})</p>
+      <div className={styles.content} dangerouslySetInnerHTML={{ __html: question.content }} />
+    </div>
+  );
+};
+
+export default QuestionCard;
